821b8fac13dacdc9d40723882a96c1ca016acc20
unsinn <fabian.maechler@gmail.com>
2013-06-11 16:27:05 +0200

    Implementing Toplist and status.

--- server.js
+++ server.js
@@ -8,53 +8,71 @@ var path = require('path');
 var adminChallengePw;
 var pwclear;
 
+var userlist = {};
+
 require('crypto').randomBytes(10, function(ex, buf) {
-	adminChallengePw = buf.toString('hex');
-	console.log(adminChallengePw);
+    adminChallengePw = buf.toString('hex');
+    console.log(adminChallengePw);
 });
 
 // listen for new web clients:
 server.listen(8080);
 
 app.get('/', function(req, res) {
-	res.sendfile('webroot/index.html');
+    res.sendfile('webroot/index.html');
 });
 
 app.use(express.static(path.join(__dirname, 'webroot')));
 
 io.sockets.on('connection', function(socket) {
 
-	socket.emit('log', {
-		text : 'Socket.io connected.'
-	});
+    socket.emit('log', {
+	text : 'Socket.io connected.'
+    });
 
-	socket.on('admin-start-cracking', function(data) {
-		if (adminChallengePw == data.adminpw) {
-			console.log("Sending start to clients: " + data.pwmd5);
-			pwclear = data.pwclear;
-			socket.broadcast.emit('clients-start-cracking', {
-				text : 'Start cracking.',
-				pwmd5 : data.pwmd5
-			});
-		} else {
-			socket.emit('log', {
-				text : 'Admin password was wrong!'
-			});
-		}
-	});
+    socket.on('admin-start-cracking', function(data) {
+	if (adminChallengePw == data.adminpw) {
+	    console.log("Sending start to clients: " + data.pwmd5);
+	    
+	    //Get the clear password and resetting the user list
+	    pwclear = data.pwclear;
+	    userlist = {};
+	    
+	    socket.broadcast.emit('clients-start-cracking', {
+		text : 'Start cracking.',
+		pwmd5 : data.pwmd5
+	    });
+	} else {
+	    socket.emit('log', {
+		text : 'Admin password was wrong!'
+	    });
+	}
+    });
 
-	socket.on('client-found-password', function(data) {
-		if (pwclear == data.password) {
-			console
-					.log("Client " + data.name
-							+ "found the correct password!!!");
-			socket.emit('log', {
-				text : 'Congratulations, you found the correct password!!!'
-			});
-		} else {
-			socket.emit('log', {
-				text : 'Sorry, you found a wrong password :('
-			});
-		}
-	});
+    socket.on('client-started-cracking', function(data) {
+	if (!userlist[this.id]) {
+	    userlist[this.id] = {
+		name : data.name,
+		status : "cracking"
+	    };
+	}
+	io.sockets.emit('userlist', userlist);
+    });
+
+    socket.on('client-found-password', function(data) {
+	var user = userlist[this.id];
+	if (pwclear == data.password) {
+	    console.log("Client " + data.name + "found the correct password!!!");
+	    user.status = "success";
+	    socket.emit('log', {
+		text : 'Congratulations, you found the correct password!!!'
+	    });
+	} else {
+	    user.status = "fail";
+	    socket.emit('log', {
+		text : 'Sorry, you found a wrong password :('
+	    });
+	}
+	io.sockets.emit('userlist', userlist);
+    });
 });
--- webroot/js/main.js
+++ webroot/js/main.js
@@ -2,83 +2,100 @@ var socket = io.connect('http://localhost:8080');
 var worker;
 
 socket.on('log', function(data) {
-	log("Node:" + data.text);
+    log("Node:" + data.text);
 });
 
 socket.on('clients-start-cracking', function(data) {
-	$('#md5').val(data.pwmd5);
-	if (worker) {
-		log("Sending start-cracking to worker");
-		worker.postMessage({
-			cmd : 'start-cracking',
-			pwmd5 : data.pwmd5
-		});
-	} else {
-		log("Error: could not start cracking. Worker not deployed");
-	}
+    $('#md5').val(data.pwmd5);
+    startWorker(data.pwmd5);
 });
 
-$(document).ready(
-		function() {
-			$("#deployWorkerButton").on(
-					"click",
-					function() {
+socket.on('userlist', function(userlist) {
+    $('#toplistTable tbody').empty();
+    for ( var key in userlist) {
+	user = userlist[key];
+	$('#toplistTable tbody').append('<tr><td>' + user.name + '</td><td>' + user.status + '</td></tr>');
+    }
+});
 
-						window.URL = window.URL || window.webkitURL;
+$(document).ready(function() {
+    $("#deployWorkerButton").on("click", function() {
 
-						// "Server response", used in all examples
-						var response = $('#webWorkerArea').val();
+	window.URL = window.URL || window.webkitURL;
 
-						var blob;
-						try {
-							blob = new Blob([ response ]);
-						} catch (e) { // Backwards-compatibility
-							window.BlobBuilder = window.BlobBuilder
-									|| window.WebKitBlobBuilder
-									|| window.MozBlobBuilder;
-							blob = new BlobBuilder();
-							blob.append(response);
-							blob = blob.getBlob();
-						}
-						worker = new Worker(URL.createObjectURL(blob));
+	// "Server response", used in all
+	// examples
+	var response = $('#webWorkerArea').val();
 
-						// Test, used in all examples:
-						worker.onmessage = function(e) {
-							if (e.data.type == 'log') {
-								log('Webworker: ' + e.data.text);
-							}
-							if (e.data.type == 'found') {
-								log('Webworker: found result = '
-										+ e.data.password);
+	var blob;
+	try {
+	    blob = new Blob([ response ]);
+	} catch (e) { // Backwards-compatibility
+	    window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
+	    blob = new BlobBuilder();
+	    blob.append(response);
+	    blob = blob.getBlob();
+	}
 
-								socket.emit('client-found-password', {
-									name : $('#cracker-name').val(),
-									password : e.data.password
-								});
-							}
-						};
-						workerStatus("Deployed");
-						crackingStatus("Idle");
-						log("Worker deployed.");
-					});
-			$("#startWorkerButton").on("click", function() {
-				var pwmd5 = $('#md5').val();
-				worker.postMessage({
-					cmd : 'start-cracking',
-					pwmd5 : pwmd5
-				});
-			});
+	try {
+	    worker = new Worker(URL.createObjectURL(blob));
 
-		});
+	    // Test, used in all examples:
+	    worker.onmessage = function(e) {
+		if (e.data.type == 'log') {
+		    log('Webworker: ' + e.data.text);
+		}
+		if (e.data.type == 'found') {
+		    log('Webworker: found result = ' + e.data.password);
+
+		    socket.emit('client-found-password', {
+			name : $('#cracker-name').val(),
+			password : e.data.password
+		    });
+		    workerStatus("Deployed / Idle / Found a password");
+		}
+	    };
+	    worker.onerror = function(e) {
+		workerStatus("Error: " + e.message);
+	    }
+	    workerStatus("Deployed");
+	    log("Worker deployed.");
+	} catch (err) {
+	    workerStatus("Error: " + err.message);
+	}
+    });
+    $("#startWorkerButton").on("click", function() {
+	var pwmd5 = $('#md5').val();
+	startWorker(pwmd5)
+    });
+
+});
+
+function startWorker(pwmd5) {
+    if (worker) {
+	sendClientStartedCracking();
+	log("Sending start-cracking to worker");
+	worker.postMessage({
+	    cmd : 'start-cracking',
+	    pwmd5 : pwmd5
+	});
+	workerStatus("Cracking...");
+    } else {
+	log("Error: could not start cracking. Worker not deployed");
+    }
+
+}
+
+function sendClientStartedCracking() {
+    socket.emit('client-started-cracking', {
+	name : $('#cracker-name').val()
+    });
+}
 
 function log(message) {
-	$('#log').append('</br>' + message);
+    $('#log').append('</br>' + message);
 }
 
 function workerStatus(message) {
-	$('#workerStatus').text(message);
-}
-
-function crackingStatus(message) {
-	$('#crackingStatus').text(message);
+    $('#workerStatus').text(message);
 }
